9a9e2864271dff7528bec005c97e0488852d94a4
Marius Tudorache <mariustudorache39@gmail.com>
2023-02-27 11:16:23 +0100

    added service test

---
+++ src/test/java/com/sanitas/calculadora/service/OperationsServiceImplTest.java
@@ -0,0 +1,36 @@
+package com.sanitas.calculadora.service;
+
+import com.sanitas.calculadora.model.Addition;
+import com.sanitas.calculadora.model.Subtraction;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+
+class OperationsServiceImplTest {
+
+    private static OperationsService operationsService;
+
+    @BeforeAll
+    public static void setup() {
+        operationsService = new OperationsServiceImpl();
+    }
+
+    /**
+     * Addition service test
+     */
+    @Test
+    public void operationsServiceExecuteAdditionTest() {
+        Assertions.assertEquals(operationsService.execute(new Addition(new BigDecimal(1), new BigDecimal(2))), new BigDecimal(3));
+    }
+
+    /**
+     * Subtraction service test
+     */
+    @Test
+    public void operationsServiceExecuteSubtractionTest() {
+        Assertions.assertEquals(operationsService.execute(new Subtraction(new BigDecimal(1), new BigDecimal(2))), new BigDecimal(-1));
+    }
+}
