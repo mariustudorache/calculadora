894c93ecf2409907e405a25175725ad8a3299bff
Marius Tudorache <mariustudorache39@gmail.com>
2023-02-27 14:42:49 +0100

    added rest controller tests

--- src/test/java/com/sanitas/calculadora/controller/OperationsControllerTest.java
+++ src/test/java/com/sanitas/calculadora/controller/OperationsControllerTest.java
@@ -1,11 +1,13 @@
 package com.sanitas.calculadora.controller;
 
 
+import com.sanitas.calculadora.controller.request.get.Operator;
 import com.sanitas.calculadora.model.Addition;
 import com.sanitas.calculadora.model.Operation;
 import com.sanitas.calculadora.model.Subtraction;
 import com.sanitas.calculadora.service.OperationsService;
 import com.sanitas.calculadora.service.OperationsServiceImpl;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -14,9 +16,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.math.BigDecimal;
 
-import static org.hamcrest.Matchers.any;
-import static org.mockito.ArgumentMatchers.anyDouble;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -31,16 +31,24 @@ public class OperationsControllerTest {
     @Mock
     private final OperationsMapper operationsMapper = mock(OperationsMapper.class);
 
+    @Test
+    public void contextLoads() throws Exception {
+        assertThat(operationsController).isNotNull();
+    }
+
 
     /**
      * Test a add operation in controller
      */
     @Test
     public void getOperationResultWithAddition() {
-        Operation operation = new Addition(new BigDecimal(5), new BigDecimal(3));
-        when(operationsMapper.toDTO(anyString(), any(Operator.class), anyString()))
+
+        Operation operation = new Addition(new BigDecimal(5), new BigDecimal(5));
+        when(operationsMapper.toDTO(new BigDecimal(5), Operator.ADD, new BigDecimal(5)))
                 .thenReturn(operation);
-        when(operationsService.execute(operation)).thenReturn(new BigDecimal(5));
+        when(operationsService.execute(operation)).thenReturn(new BigDecimal(10));
+
+        assertThat(operationsController.getOperationResult(BigDecimal.valueOf(5), Operator.ADD, BigDecimal.valueOf(5))).isEqualTo(new BigDecimal(10));
 
     }
 
@@ -50,9 +58,11 @@ public class OperationsControllerTest {
     @Test
     public void getOperationResultWithSubtraction() {
         Operation operation = new Subtraction(new BigDecimal(5), new BigDecimal(3));
-        when(operationsMapper.toDTO(anyString() , any(Operator.class), anyString()))
+        when(operationsMapper.toDTO(new BigDecimal(5), Operator.SUB, new BigDecimal(3)))
                 .thenReturn(operation);
         when(operationsService.execute(operation)).thenReturn(new BigDecimal(2));
+
+        assertThat(operationsController.getOperationResult(BigDecimal.valueOf(5), Operator.SUB, BigDecimal.valueOf(3))).isEqualTo(new BigDecimal(2));
 
     }
 
